b1f29fb5f62a538f848ff69d54fbddf513132d8e
othaderek <otha.hernandez@gmail.com>
2019-09-09 01:20:07 -0400

    Initial Commit. Cloud Functions.

---
+++ social-media-functions/functions/index.js
@@ -0,0 +1,46 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((req, res) => {
+ res.send("Hello World!");
+});
+
+exports.getPosts = functions.https.onRequest((req, res) => {
+  admin
+  .firestore()
+  .collection('posts')
+  .get()
+  .then(data => {
+    let posts = [];
+    data.forEach((doc) => {
+      posts.push(doc.data());
+    });
+    return res.json(posts);
+  })
+    .catch((err) => console.error(err));
+});
+
+exports.createPosts = functions.https.onRequest((req, res) => {
+
+  const newPost = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  admin.firestore()
+  .collection('posts')
+  .add(newPost)
+  .then((doc) => {
+    res.json({ message: `document ${doc.id} created successfully`});
+  })
+  .catch((err) => {
+    res.status(500).json({ error: 'something went wrong.'})
+    console.error(err);
+  })
+});
